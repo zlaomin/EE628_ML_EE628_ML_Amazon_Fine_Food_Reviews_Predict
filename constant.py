8f1cffff7fa1973c0920bda2d124eb534f939f8a
yuchen <zlaomamin@gmail.com>
2019-11-17 19:28:46 -0500

    rank

--- constant.py
+++ constant.py
@@ -20,14 +20,14 @@ test = [
 
 stopwords = {'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself',
              'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
-             'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these',
+             'they', 'them', 'their', 'theirs', 'themselves', 'which', 'who', 'whom', 'this', 'that', 'these',
              'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do',
              'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
-             'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before',
-             'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
-             'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each',
-             'few', 'more', 'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than',
-             'too', 'very', 's', 'can', 'will', 'just', 'don', 'should', 'now'}
+             'of', 'at', 'by', 'for', 'with', 'about', 'between', 'into', 'through', 'during', 'before',
+             'after', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'again',
+             'then', 'once', 'here', 'when', 'where', 'all', 'any', 'both', 'each',
+             'few', 'more', 'most', 'other', 'some', 'such', 'only', 'own', 'so', 'than',
+             'too', 'very', 's', 'can', 'will', 'just', 'don', 'now'}
 
 ori_stopwords = {'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself',
                  'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
